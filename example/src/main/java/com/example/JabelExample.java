a6cf47388971a03ba1f9a3e1af53c5cc86737d87
robfrank <roberto.franchini@alfresco.com>
2020-02-06 09:45:04 +0100

    cleanup

--- example/src/main/java/com/example/JabelExample.java
+++ example/src/main/java/com/example/JabelExample.java
@@ -5,11 +5,11 @@ import java.util.concurrent.Callable;
 import java.util.function.Function;
 
 public class JabelExample {
-    
+
     public static void main(String[] args) {
         System.out.println(new JabelExample().run(args));
     }
-    
+
     public String run(String[] args) {
         // Switch Expressions
         // https://openjdk.java.net/jeps/325
@@ -22,14 +22,14 @@ public class JabelExample {
         };
         return result;
     }
-    
+
     // Project Coin: Allow @SafeVarargs on private methods
     // https://bugs.openjdk.java.net/browse/JDK-7196160
     @SafeVarargs
     private String outerPrivate(List<String>... args) {
         // Look, Ma! No explicit diamond parameter
         var callable = new Callable<>() {
-            
+
             @Override
             public String call() {
                 // Var in lambda parameter
@@ -40,24 +40,24 @@ public class JabelExample {
                 return function.apply("idk ");
             }
         };
-        
+
         var closeable = new AutoCloseable() {
-            
+
             @Override
             public void close() {
-            
+
             }
         };
-        
+
         // Project Coin: Allow final or effectively final variables to be used as resources in try-with-resources
         // https://bugs.openjdk.java.net/browse/JDK-7196163
         try (closeable) {
             return callable.call();
         }
     }
-    
+
     class Inner {
-        
+
         public String innerPublic() {
             // Test nest-mate
             return outerPrivate();
