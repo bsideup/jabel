756497d923a03e0dcdff5824907bab7ddfc6e0b0
Sergei Egorov <segorov@pivotal.io>
2020-09-12 11:47:40 +0200

    Publish to Bintray

--- jabel-javac-plugin/src/main/java/com/github/bsideup/jabel/CheckSourceLevelAdvice.java
+++ jabel-javac-plugin/src/main/java/com/github/bsideup/jabel/CheckSourceLevelAdvice.java
@@ -4,10 +4,10 @@ import com.sun.tools.javac.code.Source;
 import com.sun.tools.javac.code.Source.Feature;
 import net.bytebuddy.asm.Advice;
 
-public class CheckSourceLevelAdvice {
+class CheckSourceLevelAdvice {
 
     @Advice.OnMethodEnter
-    public static void checkSourceLevel(
+    static void checkSourceLevel(
             @Advice.Argument(value = 1, readOnly = false) Feature feature
     ) {
         if (feature.allowedInSource(Source.JDK8)) {
