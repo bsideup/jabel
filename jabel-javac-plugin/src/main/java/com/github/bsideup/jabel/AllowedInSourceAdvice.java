0c1a68898748e4e6d1d40d9edc2f1c7983527714
Sergei Egorov <bsideup@gmail.com>
2021-05-31 18:10:57 +0200

    use `switch`

--- jabel-javac-plugin/src/main/java/com/github/bsideup/jabel/AllowedInSourceAdvice.java
+++ jabel-javac-plugin/src/main/java/com/github/bsideup/jabel/AllowedInSourceAdvice.java
@@ -11,22 +11,21 @@ class AllowedInSourceAdvice {
             @Advice.This Feature feature,
             @Advice.Argument(value = 0, readOnly = false) Source source
     ) {
-        String featureName = feature.name();
-        if (false
-                || featureName.equals("PRIVATE_SAFE_VARARGS")
-                || featureName.equals("SWITCH_EXPRESSION")
-                || featureName.equals("SWITCH_RULE")
-                || featureName.equals("SWITCH_MULTIPLE_CASE_LABELS")
-                || featureName.equals("LOCAL_VARIABLE_TYPE_INFERENCE")
-                || featureName.equals("VAR_SYNTAX_IMPLICIT_LAMBDAS")
-                || featureName.equals("DIAMOND_WITH_ANONYMOUS_CLASS_CREATION")
-                || featureName.equals("EFFECTIVELY_FINAL_VARIABLES_IN_TRY_WITH_RESOURCES")
-                || featureName.equals("TEXT_BLOCKS")
-                || featureName.equals("PATTERN_MATCHING_IN_INSTANCEOF")
-                || featureName.equals("REIFIABLE_TYPES_INSTANCEOF")
-        ) {
-            //noinspection UnusedAssignment
-            source = Source.DEFAULT;
+        switch (feature.name()) {
+            case "PRIVATE_SAFE_VARARGS":
+            case "SWITCH_EXPRESSION":
+            case "SWITCH_RULE":
+            case "SWITCH_MULTIPLE_CASE_LABELS":
+            case "LOCAL_VARIABLE_TYPE_INFERENCE":
+            case "VAR_SYNTAX_IMPLICIT_LAMBDAS":
+            case "DIAMOND_WITH_ANONYMOUS_CLASS_CREATION":
+            case "EFFECTIVELY_FINAL_VARIABLES_IN_TRY_WITH_RESOURCES":
+            case "TEXT_BLOCKS":
+            case "PATTERN_MATCHING_IN_INSTANCEOF":
+            case "REIFIABLE_TYPES_INSTANCEOF":
+                //noinspection UnusedAssignment
+                source = Source.DEFAULT;
+                break;
         }
     }
 }
